681453bda119d25258d13e834293fddf30ccc932
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-04 12:32:05 +0000

    [finish]2-5

---
+++ db/migrate/20220904121616_create_lists.rb
@@ -0,0 +1,16 @@
+# def change
+#  create_table : テーブル名 do |t|
+#   t.データ型 : カラム名
+#  end
+# end
+
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+      t.string:title
+      t.string:body
+     
+      t.timestamps
+    end
+  end
+end
